34512046ba33e1d83f23efc83d14ab8de64e97eb
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-03-16 02:00:17 -0300

    feat: add functionality to save robot position to a CSV file
    - Adds import of CSV module for file writing
    - Adds code to open a CSV file and write robot position data every frame
    - Modifies code to update robot positions every frame

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
+import csv
 
 from Controllers.MatchParameters import MatchParameters
 from Strategies.MainStrategy import Strategy
 
 from bridge import (Actuator, Replacer, Vision, Referee)
+
+
 
 if __name__ == "__main__":
 
@@ -27,6 +30,14 @@ if __name__ == "__main__":
     vision = Vision(isYellowTeam, "224.0.0.1", 10002)
     referee = Referee(isYellowTeam, "224.5.23.2", 10003)
 
+    filename = "dataYellowTeam.csv"
+    
+    with open(filename, mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['pos_x_1', 'pos_y_1', 'pos_x_2', 'pos_y_2', 'pos_x_3', 'pos_y_3', 'action_1', 'action_2', 'action_3'])
+        
+    actions = []
+    
     while True:
         referee.update()
         ref_data = referee.get_data()
@@ -38,10 +49,14 @@ if __name__ == "__main__":
 
         if ref_data["game_on"]:
             objectives = Strategy(teamsParameters, "default").main_strategy()
-            actuator.send_all(teamsParameters.controller(teamsParameters.yellowRobotValues
-                                                         if teamsParameters.isYellowTeam
-                                                         else teamsParameters.blueRobotValues,
-                                                         objectives))
+            actions = teamsParameters.controller(teamsParameters.yellowRobotValues if teamsParameters.isYellowTeam else teamsParameters.blueRobotValues, objectives)
+            
+            actuator.send(actions)
+
+            writer.writerow([teamsParameters.yellowRobotValues[0][0], teamsParameters.yellowRobotValues[0][1],
+                             teamsParameters.yellowRobotValues[1][0], teamsParameters.yellowRobotValues[1][1],
+                             teamsParameters.yellowRobotValues[2][0], teamsParameters.yellowRobotValues[2][1],
+                             actions[0][0], actions[1][0], actions[2][0]])
 
         elif ref_data["foul"] != 7:
             # foul behaviour
